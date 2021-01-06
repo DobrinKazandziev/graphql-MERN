daa981dac67dcd49c9c331e6a0b3cfea98f7fc62
DobrinK <dobrinkazandziev94@gmail.com>
2021-01-06 12:25:41 +0100

    user registration form, firebase confirm email

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState('');
+  
+  const handleSubmit = () => {
+
+  }
+
   return (
     <div className="container">
       <div className="row p-5">
         <h4>Login</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email Address</label>
+            <input
+              type="email" 
+              className="form-control"
+              placeholder="Enter Email"
+              disabled={loading}
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../../firebase';
 
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState('');
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const config = {
+      url: process.env.REACT_APP_CONFIRAMTION_EMAIL_REDIRECT,
+      handleCodeInApp: true,
+    }
+
+    const result = await auth.sendSignInLinkToEmail(email, config);
+    console.log(result)
+    //  show toast notification to user about email sent
+
+    //  save user email to local storage
+    window.localStorage.setItem('emailFormRegistration', email);
+    
+    //  clear state
+    setEmail('');
+    setLoading('');
+  }
+
   return (
-    <div className="container">
-      <div className="row p-5">
-        <h4>Register</h4>
-      </div>
+    <div className="container p-5">
+      <h4>Register</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="email" 
+            className="form-control"
+            placeholder="Enter Email"
+            disabled={loading}
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-raised btn-primary">Submit</button>
+      </form>
     </div>
   );
 };
