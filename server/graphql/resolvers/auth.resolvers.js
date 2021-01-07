bc0d500b700128ed070efbb7ddccba2cf07f0cb7
DobrinK <dobrinkazandziev94@gmail.com>
2021-01-07 13:45:40 +0100

    Firebase admin verify authtoken

--- server/graphql/resolvers/auth.resolvers.js
+++ server/graphql/resolvers/auth.resolvers.js
@@ -1,11 +1,13 @@
 import { gql } from 'apollo-server-express';
 import { authCheck } from '../../utils/auth';
 
+const user = async (parent, args, { req, db }, info) => {
+  await authCheck(req);
+  return 'Dobrin';
+};
+
 module.exports = {
   Query: {
-    user: (parent, args, { req, res, db }, info) => {
-      authCheck(req, res);
-      return 'Dobrin';
-    },
+    user
   }
 }
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,17 +1,36 @@
-let authorized = true;
+const admin = require("firebase-admin");
 
-const authCheck = (req, res, next = (f) => (f)) => {
-  if (!req.headers.authtoken) throw new Error('Unauthorized. Missing authtoken!');
+const serviceAccount = require("../config/firebaseServiceAccountKey.json");
 
-  //  token validity check
-  const valid = req.headers.authtoken === 'secret'
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  // databaseURL: "https://graphqlreactnode-822de-default-rtdb.europe-west1.firebasedatabase.app"
+});
 
-  if (!valid) {
-    throw new Error('Unauthorized. Invalid authtoken!')
-  } else {
-    next()
+const authCheck = async (req) => {
+  try {
+    const currentUser = await admin.auth().verifyIdToken(req.headers.authtoken);
+    console.log('CURRENT USER:', currentUser)
+    return currentUser;
+  } catch (error) {
+    console.log('AUTH CHECK ERROR:', error);
+    throw new Error(error);
   }
-};
+}
+
+
+// const authCheck = (req, res, next = (f) => (f)) => {
+//   if (!req.headers.authtoken) throw new Error('Unauthorized. Missing authtoken!');
+
+//   //  token validity check
+//   const valid = req.headers.authtoken === 'secret'
+
+//   if (!valid) {
+//     throw new Error('Unauthorized. Invalid authtoken!')
+//   } else {
+//     next()
+//   }
+// };
 
 export {
   authCheck
